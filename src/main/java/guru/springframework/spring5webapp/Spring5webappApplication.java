99419c049c5071af2093fbb2b271f5412fd296c5
derya26 <nurderya26@gmail.com>
2022-01-17 08:52:03 +0300

    Entities are added

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -3,6 +3,7 @@ package guru.springframework.spring5webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Spring5webappApplication {
 
